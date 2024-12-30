4bbeb337d70e93dca0b91e94ac8afba8063d274e
Joshua Cerdenia <joshcerdenia@gmail.com>
2024-12-31 02:03:13 +0800

    feat(subscribe): add loading text

--- src/pages/subscribe.tsx
+++ src/pages/subscribe.tsx
@@ -2,8 +2,11 @@ import Breadcrumb from "@/components/breadcrumb";
 import Layout from "@/components/layout";
 import Links from "@/components/links";
 import site from "@/data/site";
+import { useState } from "react";
 
-export default function About() {
+export default function Subscribe() {
+  const [loading, setLoading] = useState(true);
+
   return (
     <Layout
       metadata={{
@@ -18,11 +21,14 @@ export default function About() {
         <strong>Subscribe</strong>
       </h3>
 
+      {loading ? <div className="mt-4">Loading...</div> : null}
+
       <main className="mt-4">
         <iframe
           width="100%"
           height="440"
           src="https://0af2cd27.sibforms.com/serve/MUIFAEcjrGMH34rUlrJEOYGE3CtX_8G4TDkzFtD82L0L5tzgUQNSEpn8l8sh-BvmMLrI_OmZlZ0420Lq2JhgwYiXIgUqOHxfxLRMJNV5dn-l7vIEEKciI3x9-zmM1O-GTfQO0X6cs-f6RVQeEViyzgSDlIgPTnqLyI3NOp82dGuKqh3hXeFPU-XrvFAfJu6JtJt0UGXTRt0Kcp-e"
+          onLoad={() => setLoading(false)}
           style={{
             display: "block",
             marginLeft: "auto",
